a27ac8a966c485b54581aae4e7bd0bc3b31c1ec1
Bhagwati Prasad <bhagwati.indoria@gmail.com>
2023-07-28 21:04:33 +0000

    Skeleton for bit-forge

---
+++ frontend/src/js/lib.js
@@ -0,0 +1,29 @@
+const throttle = (() => {
+})();
+
+const debounce = (() => {
+})();
+
+const request = (() => {
+    const setHeader = (header, value) => {
+    }
+    const get = (url) => {
+    }
+
+    const put = (url, data) => {
+    }
+
+    const post = (url, data) => {
+    }
+
+    const delete = (url) => {
+    }
+
+    return {
+        setHeader,
+        get,
+        put,
+        post,
+        delete
+    }
+})();
---
+++ frontend/src/js/router.js
@@ -0,0 +1,8 @@
+const router = (() => {
+});
+
+const cache = (() => {
+});
+
+const prefetch = (() => {
+});
